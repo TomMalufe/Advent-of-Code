25750b3af99f47eef0193a1e49e1079058a3f306
Paul Schoneveld <paul.schoneveld@infraview.net>
2023-12-07 15:02:22 +0100

    Added Day07 Complete

--- lib/year2023/src/lib/year2023/day07/year2023-day07.component.spec.ts
+++ lib/year2023/src/lib/year2023/day07/year2023-day07.component.spec.ts
@@ -4,7 +4,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
-import { Year2023Day07Component } from './year2023-day07.component';
+import {
+  CamelCardHand,
+  HandValues,
+  Year2023Day07Component,
+} from './year2023-day07.component';
 
 describe('Year2022Day07Component', () => {
   let component: Year2023Day07Component;
@@ -29,5 +33,54 @@ describe('Year2022Day07Component', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('CamelCardHand test', () => {
+    it('should find a High Card in 42T3K', () => {
+      const hand = CamelCardHand.create('42T3K', 0);
+      expect(hand.handValue).toEqual(HandValues.HIGH_CARD);
+    });
+    it('should find a pair in 32T3K', () => {
+      const hand = CamelCardHand.create('32T3K', 0);
+      expect(hand.handValue).toEqual(HandValues.ONE_PAIR);
+    });
+    it('should find a pair in KK677', () => {
+      const hand = CamelCardHand.create('KK677', 0);
+      expect(hand.handValue).toEqual(HandValues.TWO_PAIR);
+    });
+    it('should find Three of a Kind in T55J5', () => {
+      const hand = CamelCardHand.create('T55J5', 0);
+      expect(hand.handValue).toEqual(HandValues.FOUR_OF_A_KIND);
+    });
+    it('should find a pair in TTJJT', () => {
+      const hand = CamelCardHand.create('TTJJT', 0);
+      expect(hand.handValue).toEqual(HandValues.FIVE_OF_A_KIND);
+    });
+    it('should find a pair in QQQQA', () => {
+      const hand = CamelCardHand.create('QQQQA', 0);
+      expect(hand.handValue).toEqual(HandValues.FOUR_OF_A_KIND);
+    });
+    it('should find a pair in JJJJJ', () => {
+      const hand = CamelCardHand.create('JJJJJ', 0);
+      expect(hand.handValue).toEqual(HandValues.FIVE_OF_A_KIND);
+    });
+
+    it('should be able to compare two hands', () => {
+      const handA = CamelCardHand.create('T55J5', 0);
+      const handB = CamelCardHand.create('KK677', 0);
+      expect(handA.compare(handB)).toBeGreaterThan(0);
+    });
+
+    it('should be able to compare two similar hands', () => {
+      const handA = CamelCardHand.create('KK677', 0);
+      const handB = CamelCardHand.create('KK877', 0);
+      expect(handA.compare(handB)).toBeLessThan(0);
+    });
+
+    it('should be able to compare two similar hands', () => {
+      const handA = CamelCardHand.create('55577', 0);
+      const handB = CamelCardHand.create('J5577', 0);
+      expect(handA.compare(handB)).toBeGreaterThan(0);
+    });
   });
 });
--- lib/year2023/src/lib/year2023/day07/year2023-day07.component.ts
+++ lib/year2023/src/lib/year2023/day07/year2023-day07.component.ts
@@ -3,6 +3,132 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 
+type CardType =
+  | 'A'
+  | 'K'
+  | 'Q'
+  | 'T'
+  | '9'
+  | '8'
+  | '7'
+  | '6'
+  | '5'
+  | '4'
+  | '3'
+  | '2'
+  | 'J';
+const CARD_VALUES: Record<CardType, number> = {
+  A: 12,
+  K: 11,
+  Q: 10,
+  T: 9,
+  '9': 8,
+  '8': 7,
+  '7': 6,
+  '6': 5,
+  '5': 4,
+  '4': 3,
+  '3': 2,
+  '2': 1,
+  J: 0,
+};
+export enum HandValues {
+  HIGH_CARD,
+  ONE_PAIR,
+  TWO_PAIR,
+  THREE_OF_A_KIND,
+  FULL_HOUSE,
+  FOUR_OF_A_KIND,
+  FIVE_OF_A_KIND,
+}
+
+export class CamelCardHand {
+  private _cards: Record<CardType, number> = {
+    A: 0,
+    K: 0,
+    Q: 0,
+    J: 0,
+    T: 0,
+    '9': 0,
+    '8': 0,
+    '7': 0,
+    '6': 0,
+    '5': 0,
+    '4': 0,
+    '3': 0,
+    '2': 0,
+  };
+  get hand(): string {
+    return this._hand;
+  }
+  get bet(): number {
+    return this._bet;
+  }
+  private _handValue: HandValues = 0;
+  get handValue(): HandValues {
+    return this._handValue;
+  }
+  compare(handB: CamelCardHand): number {
+    if (this.handValue === handB.handValue) {
+      let i = 0;
+      while (
+        i < this.hand.length &&
+        CARD_VALUES[this.hand.charAt(i) as CardType] ===
+          CARD_VALUES[handB.hand.charAt(i) as CardType]
+      ) {
+        i++;
+      }
+      if (i >= this.hand.length) {
+        return 0;
+      }
+      return (
+        CARD_VALUES[this.hand.charAt(i) as CardType] -
+        CARD_VALUES[handB.hand.charAt(i) as CardType]
+      );
+    }
+    return this.handValue - handB.handValue;
+  }
+
+  private constructor(private _hand: string, private _bet: number) {
+    for (let i = 0; i < _hand.length; i++) {
+      this._cards[_hand.charAt(i) as CardType]++;
+    }
+    const JOKERS = this._cards.J;
+    this._cards.J = 0;
+    const counts = Object.values(this._cards)
+      .filter((it) => it > 0)
+      .sort((a, b) => b - a);
+    switch ((counts[0] || 0) + JOKERS) {
+      case 5:
+        this._handValue = HandValues.FIVE_OF_A_KIND;
+        break;
+      case 4:
+        this._handValue = HandValues.FOUR_OF_A_KIND;
+        break;
+      case 3:
+        if (counts[1] === 2) {
+          this._handValue = HandValues.FULL_HOUSE;
+        } else {
+          this._handValue = HandValues.THREE_OF_A_KIND;
+        }
+        break;
+      case 2:
+        if (counts[1] === 2) {
+          this._handValue = HandValues.TWO_PAIR;
+        } else {
+          this._handValue = HandValues.ONE_PAIR;
+        }
+        break;
+      default:
+        this._handValue = HandValues.HIGH_CARD;
+    }
+  }
+
+  static create(hand: string, bet: number): CamelCardHand {
+    return new CamelCardHand(hand, bet);
+  }
+}
+
 @Component({
   selector: 'advent-year2023-day07',
   standalone: true,
@@ -15,9 +141,19 @@ export class Year2023Day07Component {
   result2 = '';
 
   processInput(value: string): void {
-    const lines = value.split('\n');
+    const matches = value.matchAll(/([2-9AKQJT]{5}) (\d+)/g);
 
-    this.result1 = '';
+    const hands = Array.from(matches, (it) =>
+      CamelCardHand.create(it[1], Number.parseInt(it[2]))
+    );
+    hands.sort((a, b) => a.compare(b));
+    const winnings = hands.reduce(
+      (previousValue, currentValue, currentIndex) =>
+        previousValue + currentValue.bet * (currentIndex + 1),
+      0
+    );
+
+    this.result1 = '' + winnings;
     this.result2 = '';
   }
 }
