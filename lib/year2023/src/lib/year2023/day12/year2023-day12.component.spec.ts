14d722b6d9ca14fcebc951ec1417cb1caa6ca457
Paul Schoneveld <paul.schoneveld@infraview.net>
2023-12-12 16:43:02 +0100

    Added 2023Day12 Part 1 solution

--- lib/year2023/src/lib/year2023/day12/year2023-day12.component.spec.ts
+++ lib/year2023/src/lib/year2023/day12/year2023-day12.component.spec.ts
@@ -30,4 +30,100 @@ describe('Year2023Day12Component', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('given some simple test cases', () => {
+    it('should find 3 arrangement for "??? 1"', () => {
+      expect(component.countArrangements('??? 1')).toEqual(3);
+    });
+    it('should find 2 arrangement for "??? 2"', () => {
+      expect(component.countArrangements('??? 2')).toEqual(2);
+    });
+    it('should find 1 arrangement for "?#? 1"', () => {
+      expect(component.countArrangements('?#? 1')).toEqual(1);
+    });
+    it('should find 2 arrangement for "?#? 2"', () => {
+      expect(component.countArrangements('?#? 2')).toEqual(2);
+    });
+
+    it('should find 1 arrangement for "??? 1,1"', () => {
+      expect(component.countArrangements('??? 1,1')).toEqual(1);
+    });
+    it('should find 3 arrangement for "???? 1,1"', () => {
+      expect(component.countArrangements('???? 1,1')).toEqual(3); // #.#. / #..# / .#.#
+    });
+    it('should find 2 arrangement for "???# 1,1"', () => {
+      expect(component.countArrangements('???# 1,1')).toEqual(2);
+    });
+    it('should find 1 arrangement for "???? 1,2"', () => {
+      expect(component.countArrangements('???? 1,2')).toEqual(1);
+    });
+
+    it('should find 2 arrangement for "?.? 1"', () => {
+      expect(component.countArrangements('?.? 1')).toEqual(2);
+    });
+  });
+
+  describe('given the test data', () => {
+    it('should find 1 arrangement for "???.### 1,1,3"', () => {
+      expect(component.countArrangements('???.### 1,1,3')).toEqual(1);
+    });
+    it('should find 1 arrangement for "????.#...#... 4,1,1"', () => {
+      expect(component.countArrangements('????.#...#... 4,1,1')).toEqual(1);
+    });
+    it('should find 4 arrangement for ".??..??...?##. 1,1,3"', () => {
+      expect(component.countArrangements('.??..??...?##. 1,1,3')).toEqual(4);
+    });
+    it('should find 1 arrangement for "?#?#?#?#?#?#?#? 1,3,1,6"', () => {
+      expect(component.countArrangements('?#?#?#?#?#?#?#? 1,3,1,6')).toEqual(1);
+    });
+    it('should find 4 arrangement for "????.######..#####. 1,6,5"', () => {
+      expect(component.countArrangements('????.######..#####. 1,6,5')).toEqual(
+        4
+      );
+    });
+    it('should find 10 arrangement for "?###???????? 3,2,1"', () => {
+      expect(component.countArrangements('?###???????? 3,2,1')).toEqual(10);
+    });
+    it('should find 22 arrangement for "???.????????#.. 1,1,1,2,1"', () => {
+      expect(component.countArrangements('???.????????#.. 1,1,1,2,1')).toEqual(
+        22
+      );
+    });
+    it('should find 1 arrangement for "??#?##?#?. 1,5"', () => {
+      expect(component.countArrangements('??#?##?#?. 1,5')).toEqual(1);
+    });
+
+    it('should find 3 arrangement for "??#???? 3"', () => {
+      expect(component.countArrangements('??#???? 3')).toEqual(3);
+    });
+  });
+
+  describe('given the test data for Part 2', () => {
+    it('should find 1 arrangement for "???.### 1,1,3"', () => {
+      expect(component.countArrangementsV2('???.### 1,1,3')).toEqual(1);
+    });
+    it('should find 16 arrangement for "????.#...#... 4,1,1"', () => {
+      expect(component.countArrangementsV2('????.#...#... 4,1,1')).toEqual(16);
+    });
+    it('should find 16384 arrangement for ".??..??...?##. 1,1,3"', () => {
+      expect(component.countArrangementsV2('.??..??...?##. 1,1,3')).toEqual(
+        16384
+      );
+    });
+    it('should find 1 arrangement for "?#?#?#?#?#?#?#? 1,3,1,6"', () => {
+      expect(component.countArrangementsV2('?#?#?#?#?#?#?#? 1,3,1,6')).toEqual(
+        1
+      );
+    });
+    it('should find 2500 arrangement for "????.######..#####. 1,6,5"', () => {
+      expect(
+        component.countArrangementsV2('????.######..#####. 1,6,5')
+      ).toEqual(2500);
+    });
+    it('should find 506250 arrangement for "?###???????? 3,2,1"', () => {
+      expect(component.countArrangementsV2('?###???????? 3,2,1')).toEqual(
+        506250
+      );
+    });
+  });
 });
--- lib/year2023/src/lib/year2023/day12/year2023-day12.component.ts
+++ lib/year2023/src/lib/year2023/day12/year2023-day12.component.ts
@@ -1,3 +1,4 @@
+import { sumAll } from '@advent/shared';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
@@ -14,8 +15,58 @@ import { DayTemplateComponent } from '../day-template.component';
 export class Year2023Day12Component extends DayTemplateComponent {
   processInput(value: string): void {
     const lines = value.split('\n');
+    const start = Date.now();
 
-    this.result1 = '';
-    this.result2 = '';
+    const counts = lines.map(this.countArrangements);
+    const countsV2 = lines.map(this.countArrangementsV2);
+
+    this.result1 = '' + sumAll(counts);
+    this.result2 = '' + sumAll(countsV2);
+    console.log(Date.now() - start);
+  }
+
+  countArrangements(line: string): number {
+    const [springs, brokenString] = line.split(' ');
+    const groupSizes = brokenString.split(',').map((it) => Number.parseInt(it));
+
+    let arrangements = 0;
+    const search = (searchString: string, groups: number[]) => {
+      const regex = new RegExp('(?<!#)[#?]{' + groups[0] + '}(?!#)');
+      let matchIndex = searchString.search(regex);
+      if (matchIndex === -1) {
+        return;
+      }
+      if (/#/.test(searchString.substring(0, matchIndex))) {
+        return;
+      }
+      if (groups.length > 1) {
+        search(
+          searchString.substring(matchIndex + groups[0] + 1),
+          groups.slice(1)
+        );
+      } else if (
+        !/#/.test(searchString.substring(matchIndex + groups[0] + 1))
+      ) {
+        arrangements++;
+      }
+      if (searchString.charAt(matchIndex) !== '#') {
+        search(searchString.substring(matchIndex + 1), groups);
+      }
+    };
+    search(springs, groupSizes);
+
+    return arrangements;
+  }
+
+  countArrangementsV2(line: string): number {
+    const [springs, brokenString] = line.split(' ');
+    const unfoldedSprings = [0, 1, 2, 3, 4].map(() => springs).join('?');
+    const groupSizes = [0, 1, 2, 3, 4]
+      .map(() => brokenString)
+      .join(',')
+      .split(',')
+      .map((it) => Number.parseInt(it));
+
+    return 0;
   }
 }
